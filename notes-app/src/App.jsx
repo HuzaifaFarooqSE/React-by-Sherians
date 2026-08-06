2901772c475912adc1534b37a7aa9806b3f63188
HuzaifaFarooqSE <malikhuzaifa1309@gmail.com>
2026-08-06 17:16:55 +0000

    setup

---
+++ notes-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <form className='flex justify-between'>
+            <input type="text" placeholder='Enter your tasks' className='inp' />
+            <textarea name="" id="">Enter your text here</textarea>
+      </form>
+    </>
+  )
+}
+
+export default App
