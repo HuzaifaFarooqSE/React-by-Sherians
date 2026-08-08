9416a40f73f894f5ef8e0c97993a420306eaddd1
HuzaifaFarooqSE <malikhuzaifa1309@gmail.com>
2026-08-08 20:51:56 +0500

    Done For Now (Maybe work on it later)

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,48 +1,90 @@
 import React  from 'react'
 import { useState } from "react";
 
-const formHandler = (e) =>{
-  e.preventDefault()
-  console.log("Form Submitted")
-}
+
 const App = () => {
-  const [color, setcolor] = useState('bg-white')
-  const changeColor = ()=>{
-    setcolor('bg-green-400')
-  }
+  const [title, setTitle] = useState('')
+  const [details, setdetails] = useState('')
+  const [task, setTask] = useState([])
+  const formHandler = (e) =>{
+  e.preventDefault()
+  
+  // console.log(title)
+  // console.log(details)
+  const copyTask = [...task]
+  copyTask.push({ title, details })
+  setTask(copyTask)
+  console.log(copyTask)
+  setTitle('');
+  setdetails('')
+  // const copyTask = [...task]
+
+  //   copyTask.push({ title, details })
+
+  //   setTask(copyTask)
+
+  //   console.log(copyTask)
+
+  //   setTitle('')
+  //   setdetails('')
+}
   return (
     <div className='h-screen lg:flex bg-black text-white'>
-      <form onSubmit={(e)=>{
-        formHandler(e)
-      }} className='flex gap-4 lg:w-1/2 p-10 flex-col items-start'>
+
+
+          <form onSubmit={(e)=>{
+            
+          formHandler(e)
+          }} className='flex gap-4 lg:w-1/2 p-10 flex-col items-start'>
+
+
          <h1 className='text-4xl mb-2 font-bold'>Add Notes</h1>
+
             <div  className='flex flex-col items-start gap-5 w-1/2'>
+
               <input type="text" 
                 placeholder='Enter your tasks' 
-                className='px-5 w-full font-medium py-2 border-2 outline-none rounded' />
+                className='px-5 w-full font-medium py-2 border-2 outline-none rounded'
+                value={title}
+                onChange={(e)=>{
+                  setTitle(e.target.value)
+                }} />
+
               <textarea 
                 placeholder='Write details' 
-                className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded' />
+                className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded'
+                value={details}
+                onChange={(e)=>{
+                  setdetails(e.target.value)
+                }} />
+
               <button 
-              className='bg-white active:scale-95 font-medium w-full outline-none  text-black px-5 py-2 rounded'>Add Note</button>
+              className='bg-white active:bg-gray-400 font-medium w-full outline-none  text-black px-5 py-2 rounded'>Add Note</button>
               
             </div>
             {/* <img className='h-56' src="https://imgs.search.brave.com/yYZbe2je2gGM2utwDlhxniTUrTSF8JaBGSeGMcF6_8s/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNDkv/NTc4LzE1NS9zbWFs/bC9hLWJsYWNrLWFu/ZC13aGl0ZS1kcmF3/aW5nLW9mLWEtbWFu/LXdyaXRpbmctcG5n/LnBuZw" alt="" /> */}
 
             
       </form>
-      <div className='flex gap-5 flex-wrap'>
-        <div className={`h-52 w-40 ${color} rounded-2xl`}>
-          <button onClick={changeColor}>Click</button>
-        </div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
-        <div className='h-52 w-40 rounded-2xl bg-white'></div>
+
+      <div className='lg:w-1/2 lg:border-l p-10'>
+
+        <h1 className='text-xl'>My Notes</h1>
+        
+      <div className='h-full mt-5 overflow-auto flex gap-5 flex-wrap'>
+        
+        {task.map((elem, idx)=>{
+          return(
+            <div key={idx} className='h-52 w-40 rounded-2xl bg-white text-black p-4'>
+              <h3>{elem.title}</h3>
+              <h3>{elem.details}</h3>
+            </div>
+          )
+        })}
+     
+       
+        
+      </div>
       </div>
     </div>
   )
