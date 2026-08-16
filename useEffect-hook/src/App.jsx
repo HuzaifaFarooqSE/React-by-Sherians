56b10466114b5677f350b490459f624e3c7232f7
HuzaifaFarooqSE <malikhuzaifa1309@gmail.com>
2026-08-16 20:44:36 +0500

    final commit on this laptop

--- useEffect-hook/src/App.jsx
+++ useEffect-hook/src/App.jsx
@@ -1,25 +1,53 @@
+// import React, {useState} from 'react'
+// import { useEffect } from 'react'
+// const App = () => {
+//   const [num, setnum] = useState(0)
+//   const [num2, setnum2] = useState(100)
+//   useEffect(function(){
+    
+//     console.log('Useeffect is in use')
+//   },[num2])
+//   return (
+//     <>
+//     <div>{num}</div>
+//     <div>{num2}</div>
+//     <button 
+//     onMouseEnter={ ()=>{
+//       setnum(num+1)
+//     }
+//     }
+//     onMouseLeave={ () =>{
+//       setnum2(num2+10)
+//     }
+//     }>click me </button>
+//     </>
+//   )
+// }
+
+// export default App
+// Use Effect practice
 import React, {useState} from 'react'
 import { useEffect } from 'react'
 const App = () => {
-  const [num, setnum] = useState(0)
-  const [num2, setnum2] = useState(100)
-  useEffect(function(){
+  const [count, setcount] = useState(0)
+
+  const increment = () =>{
+    setcount(count+1)
+  }
+
+  useEffect(() => {
     
-    console.log('Useeffect is in use')
-  },[num2])
+  console.log("counter increased");
+  
+    
+  }, [count])
+  
   return (
     <>
-    <div>{num}</div>
-    <div>{num2}</div>
-    <button 
-    onMouseEnter={ ()=>{
-      setnum(num+1)
-    }
-    }
-    onMouseLeave={ () =>{
-      setnum2(num2+10)
-    }
-    }>click me </button>
+    <div>
+      {count}
+    </div>
+    <button onClick={increment}>Click</button>
     </>
   )
 }
